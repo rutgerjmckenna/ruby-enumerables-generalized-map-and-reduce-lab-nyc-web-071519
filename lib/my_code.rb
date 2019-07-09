a56c1c3472298f7fa76b973c155fb2aa76b28011
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-07-09 18:52:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
-# Your Code Here
+def map(array)
+  new_array = []
+  i = 0 
+  while i < array.length
+  new_array << yield(array[i])
+  i += 1
+  end
+  return new_array
+end
+
+def reduce(source_array)
+  running_total = 0
+  i = 0
+  while i < source_array.length
+  running_total << yield (source_array[i])
+  i += 1
+  end
+  return running_total
+end
