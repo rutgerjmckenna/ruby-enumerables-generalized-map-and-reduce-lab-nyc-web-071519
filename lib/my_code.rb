da6f3c49bbf1c54fdc945a20384016e1a158c42c
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-07-09 19:03:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,9 +13,11 @@ def reduce(source_array, starting_point = nil)
     running_total = starting_point
     i = 0
   else
-    running_total = 
+    running_total = source_array[0]
+    i = 1
+  end
     while i < source_array.length
-  #running_total << yield (source_array[i])
+  running_total = yield (running_total, source_array[i])
   i += 1
   end
   return running_total
